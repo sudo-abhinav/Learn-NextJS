e929c1e1a2812884e4f2dd20388d94d4ca743001
sudo-abhinav <presonalabhi@gmail.com>
2024-05-17 04:00:21 +0530

    added  s3 multer router

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -10,6 +10,8 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { text } from 'stream/consumers';
 import { UploadService } from './upload.service';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import multer from 'multer';
 
 @Controller('upload')
 export class UploadController {
@@ -17,16 +19,29 @@ export class UploadController {
   // ? calling service here for sending file
 
   @Post('aws-s3')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+        // name:'string'
+      },
+    },
+  })
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(
     // ? Function name ||
     @UploadedFile(
       new ParseFilePipe({
         validators: [
-        //   new MaxFileSizeValidator({
-        //     maxSize: 5000,
-        //     // ? this is file size valiadator
-        //   }),
+          //   new MaxFileSizeValidator({
+          //     maxSize: 5000,
+          //     // ? this is file size valiadator
+          //   }),
           // new FileTypeValidator({
           //     fileType: 'image/jpg,png',
           // ? this is inbuilt validator
@@ -37,8 +52,7 @@ export class UploadController {
     file: Express.Multer.File,
   ) {
     // console.log(file);
-    return await this.uploadService.upload(file.filename , file.buffer)
+    return await this.uploadService.upload(file.originalname, file.buffer);
     // return sta
-    
   }
 }
--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 
+
+// ! here we are using rate limiting using nest throttler
 @Module({
   controllers: [UploadController],
   providers: [UploadService]
